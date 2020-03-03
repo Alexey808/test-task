21b42ffd1030cc273a5acb6b18aea127519d28b6
Alexey Svetinskiy <alexey.svetinskiy@cosysoft.ru>
2020-03-03 09:55:01 +0400

    add tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { SharedModule } from './components/common/shared.module';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -7,6 +8,7 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      imports: [ SharedModule ]
     }).compileComponents();
   }));
 
@@ -14,18 +16,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'test-task'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('test-task');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('test-task app is running!');
   });
 });
--- src/app/components/common/input/input.component.spec.ts
+++ src/app/components/common/input/input.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { InputComponent } from './input.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
 
 describe('InputComponent', () => {
   let component: InputComponent;
@@ -8,7 +10,8 @@ describe('InputComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ InputComponent ]
+      declarations: [ InputComponent ],
+      imports: [ ReactiveFormsModule, BrowserDynamicTestingModule ]
     })
       .compileComponents();
   }));
@@ -19,7 +22,30 @@ describe('InputComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should component create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialisation control form', () => {
+    const formGroup = component.initForm(null);
+    const inputValue = formGroup.get('inputValue').value;
+    expect(inputValue).toBeNull();
+  });
+
+  it('should emit value', () => {
+    let result = null;
+    component.form.get('inputValue').setValue('test');
+    fixture.componentInstance.currentValue.subscribe(valueEmit => {
+      result = valueEmit;
+    });
+    component.emitValue(component.form.get('inputValue').value);
+    expect(result).toContain('test');
+  });
+
+  it('should by valid input value', () => {
+    component.minLength = 3;
+    component.maxLength = 10;
+    const formGroup = component.initForm('test');
+    expect(formGroup.valid).toBeTruthy();
   });
 });
